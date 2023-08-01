5166a957728e806e7158850e2803acffd9dda397
PotatoUser69 <totti312002@gmail.com>
2023-08-01 15:24:12 +0100

    finished section 9

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,6 +16,5 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
-    path('',include("core.urls")),
     path('admin/', admin.site.urls),
 ]
