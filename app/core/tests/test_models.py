c50ecbadb4c4d708adb2999d82eadef08b2357c6
PotatoUser69 <totti312002@gmail.com>
2023-08-01 14:42:20 +0100

    section 9 04

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,20 +1,18 @@
 from django.test import TestCase
-from core.models import Writer,Book
-from core.serilazers import WriterSerializer,BookSerializer
+from django.contrib.auth import get_user_model
 
-Writer_URL=reverse('recipe:recipe-list')
 
-def create_autor(self):
-    name='test'
-    last_name="last_test"
-    return Writer.objects.create(name=name,last_name=last_name)
+class ModelTests(TestCase):
+    """Test models."""
 
-class ModelTest(TestCase):
-    def test_create_writer(self):
-        create_autor()
-        create_autor()
-        res = self.client.get(Writer_URL)
-        data=Writer.objects.all()
-        serializer=WriterSerializer(data=data,many=True)
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.assertEqual(res.data, serializer.data)
+    def test_create_user_with_email_successful(self):
+        """Test creating a user with an email is successful."""
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
